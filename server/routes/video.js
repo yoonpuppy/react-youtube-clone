9039e758725268b347f4faf62f80079944ec7141
parkcheodol <3611345@naver.com>
2020-07-23 19:26:38 +0900

    200723 8 LandingPage Video

--- server/routes/video.js
+++ server/routes/video.js
@@ -56,12 +56,22 @@ router.post('/uploadVideo', (req, res) => {
     })
 })
 
+// #8 6:00 /api/video/getVideos
+router.get('/getVideos', (req, res) => {
+    // 비디오를 DB에서 가져와서 클라이언트에 보낸다
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err)
+            res.status(200).json({ success : true, videos})
+        })
+})
+
 
 // #6 4:30
 router.post('/thumbnail', (req, res) => {
     
     // 썸네일 생성 후 비디오 러닝타임 가져오기
-
     let filePath = ""
     let fileDuration = ""
     
